78b63a122e9242f2ff6b26133688f8a21d2912f2
Rahul_Raj <147519820+RahulRaj151@users.noreply.github.com>
2025-05-15 13:05:54 +0530

    Delete Semester_4/MWDW/Assignment 5 directory

--- Semester_4/MWDW/Assignment 5/1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$num = 100;
-while ($num >= 50) {
-    if ($num % 2 != 0) {
-        echo $num . " ";
-    }
-    $num--;
-}
-?>
--- Semester_4/MWDW/Assignment 5/6.php
+++
@@ -1,8 +0,0 @@
-<?php
-$students = array("John", "Emma", "Michael", "Sarah", "David");
-
-echo "Student Names:<br>";
-for ($i = 0; $i < count($students); $i++) {
-    echo ($i + 1) . ". " . $students[$i] . "<br>";
-}
-?>
