e81fb1b8cbef1d056afc58a41fd58dea453eebcf
Rahul_Raj <147519820+RahulRaj151@users.noreply.github.com>
2025-05-15 17:43:37 +0530

    Update 2.php

--- Semester_4/MWDW/Assignment_5/2.php
+++ Semester_4/MWDW/Assignment_5/2.php
@@ -5,6 +5,6 @@ $hourly_rate = 15;
 
 $salary = $working_hours * $hourly_rate;
 
-echo "Employee ID: " . $employee_id . "<br>";
+echo "Employee ID: " . $employee_id . "\n";
 echo "Salary: $" . $salary;
 ?>
